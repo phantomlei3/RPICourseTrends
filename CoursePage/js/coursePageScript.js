7199f6b532448785638d9e0a3562a2bd8bd79e74
alexZr <zhur3@rpi.edu>
2018-09-30 22:16:31 -0400

    updated coursePage so it looks closer to the latest design draft

--- CoursePage/js/coursePageScript.js
+++ CoursePage/js/coursePageScript.js
@@ -14,10 +14,10 @@ $.getJSON("assets/courseData.json",function(dtaCourse, status) {
         console.log(status);
         return;
     }
-    $("#departName").append("Department: "+dtaCourse.department);
+    $("#departName").append("Course Department: "+dtaCourse.department);
     for (let pair in dtaCourse) {
         if (dtaCourse[pair]["courseCode"] != undefined) {
-            $(".contents").append("<div class=course><div class=courseNo>"+dtaCourse[pair]["courseCode"]+"</div><div class=professor>"+dtaCourse[pair]["professor"]+"</div></div>");
+            $(".contents").append("<div class=course><div class=courseNo>"+dtaCourse[pair]["courseCode"]+"\t"+dtaCourse[pair]["courseName"]+"</div><div class=professor>"+"Instructor(s): "+dtaCourse[pair]["professor"]+"</div></div>");
         }
     }    
     alert("开机速度优化成功！");
