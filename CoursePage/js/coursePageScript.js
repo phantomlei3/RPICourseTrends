fbcfe922b76355e2f5f9e49a0e8caed36262d3ad
alexZr <zhur3@rpi.edu>
2018-10-07 20:56:07 -0400

    in b4 refactoring using Vue

--- CoursePage/js/coursePageScript.js
+++ CoursePage/js/coursePageScript.js
@@ -3,6 +3,20 @@ function change() {
     console.log("done");
 }
 
+//---------------------------------------------------------------------
+
+// decide what's the current semester
+let today = new Date();
+let subSemester = "";
+if (today.getMonth() <= 5) {    ///todo: double-check when summer ARCH starts and fix this
+    subSemester = "Spring ";
+}else if (today.getMonth() >= 9) {
+    subSemester = "Fall ";
+}else{
+    subSemester = "Summer ";
+}
+$("#currentSemester").text(subSemester + today.getFullYear());
+
 // setup and load course data from json
 let dtaCourse = "";
 $.getJSON("assets/courseData.json",function(dtaCourse, status) {
@@ -10,7 +24,7 @@ $.getJSON("assets/courseData.json",function(dtaCourse, status) {
         console.log(status);
         return;
     }
-    $("#departName").append("Course Department: "+dtaCourse.department);
+    $("#departName").append("<strong>Course Department: "+dtaCourse.department+"</strong>");
     for (let pair in dtaCourse) {
         if (dtaCourse[pair]["courseCode"] != undefined) {
             $(".contents").append("<div class=course><div class=courseNo>"+dtaCourse[pair]["courseCode"]+"\t"+dtaCourse[pair]["courseName"]+"</div><div class=professor>"+"Instructor(s): "+dtaCourse[pair]["professor"]+"</div></div>");
